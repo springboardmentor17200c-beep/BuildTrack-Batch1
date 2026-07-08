2391c8b3d063689ddd94d78c94004bb3482ddb54
Prem kumar <prem857901@gmail.com>
2026-07-08 11:20:59 +0530

    feat: redesign dashboard UI

--- frontend/buildtrack-frontend/src/app/app.routes.ts
+++ frontend/buildtrack-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { DashboardComponent } from './features/dashboard/dashboard';
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  }
+];
